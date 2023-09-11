6643e69ce89144876bda442e61eb5ba257221dab
Bal Narendra Sapa <bnsapa2000@gmail.com>
2023-09-11 21:59:02 +0000

    add userlist apiview

--- django_api/userops/views.py
+++ django_api/userops/views.py
@@ -1,9 +1,27 @@
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import authentication, permissions
 from django.shortcuts import render
-from rest_framework import viewsets
 from .serializers import UserSerializer
 from django.contrib.auth.models import User
+from rest_framework import status
 # Create your views here.
 
-class UserViewSet(viewsets.ModelViewSet):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+class UserList(APIView):
+    """
+    List all users, or create a new user.
+    """
+    def get(self, request, format=None):
+        users = User.objects.all()
+        print(users)
+        serializer = UserSerializer(users, many=True)
+        print(serializer.data)
+        # return Response(serializer.data)
+        return Response({"users": serializer.data})
+
+    def post(self, request, format=None):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            # serializer.save()
+            return Response({"users": serializer.data})
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
