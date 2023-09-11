d538230f46aa9185f60077caab4724871e0504fb
Bal Narendra Sapa <bnsapa2000@gmail.com>
2023-09-11 21:58:43 +0000

    add apiview code adjustments

--- django_api/userops/urls.py
+++ django_api/userops/urls.py
@@ -1,12 +1,6 @@
 from django.urls import path, include
-from rest_framework import routers, serializers
-from .views import UserViewSet
-
-# Routers provide an easy way of automatically determining the URL conf.
-router = routers.DefaultRouter()
-router.register(r'users', UserViewSet)
+from .views import *
 
 urlpatterns = [
-    path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('', UserList.as_view()),
 ]
