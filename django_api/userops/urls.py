04553e560f0d7d0d761b55aafdc67c20a37a153e
Bal Narendra Sapa <bnsapa2000@gmail.com>
2023-09-11 22:29:11 +0000

    add jwt routes

--- django_api/userops/urls.py
+++ django_api/userops/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path, include
 from .views import *
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('', Userops.as_view()),
+    path('login/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
