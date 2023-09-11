923b23d3c1355b213d57cfc55b02cba991c332b5
Bal Narendra Sapa <bnsapa2000@gmail.com>
2023-09-11 21:57:45 +0000

    comment permissions read only in settings

--- django_api/django_api/settings.py
+++ django_api/django_api/settings.py
@@ -41,13 +41,13 @@ INSTALLED_APPS = [
     "userops"
 ]
 
-REST_FRAMEWORK = {
-    # Use Django's standard `django.contrib.auth` permissions,
-    # or allow read-only access for unauthenticated users.
-    'DEFAULT_PERMISSION_CLASSES': [
-        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
-    ]
-}
+# REST_FRAMEWORK = {
+#     # Use Django's standard `django.contrib.auth` permissions,
+#     # or allow read-only access for unauthenticated users.
+#     'DEFAULT_PERMISSION_CLASSES': [
+#         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+#     ]
+# }
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
